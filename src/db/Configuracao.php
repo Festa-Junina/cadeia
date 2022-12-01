f6e8b107174a14bf70d9b08cbb64d469335b353d
phgiacomelli <101350035+phgiacomelli@users.noreply.github.com>
2022-12-01 14:57:00 -0300

    updates

--- src/db/Configuracao.php
+++ src/db/Configuracao.php
@@ -3,4 +3,9 @@
     define("USUARIO","root");
     define("SENHA","");
     define("BANCO","  ");
+
+    define("HOST","192.168.103.223/phpmyadmin");
+    define("USUARIO","aluno");
+    define("SENHA","aluno");
+    define("BANCO","cadeia-app");
 ?>
