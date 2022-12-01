17e1f1d4030e18c6c99a523196fc701b469449c5
marceloost <marcelo.ost7@gmail.com>
2022-12-01 11:35:48 -0300

    up

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ if(isset($_POST['botao'])){
     require_once __DIR__."/vendor/autoload.php";
     $carc = new Carcereiro($_POST['email'],$_POST['senha']);
     if($carc->authenticate()){
-        header("location: restrita.php");
+        header("location: listarCarc.php");
     }else{
         header("location: index.php");
     }
@@ -19,21 +19,21 @@ if(isset($_POST['botao'])){
     <title>Carcereiro</title>
 </head>
 <body>
-    <div class="formulario1">
+    <div class="formulario">
         <form action="index.php" method='post'>
                 <label for='email'>E-mail:</label>
                 <br>
                 <input type='email' name='email' id='email' required>
                 <br>
-                <label for='password'>Password:</label>
+                <label for='senha'>Senha:</label>
                 <br>
-                <input type='password' name='password' id='password' required>
+                <input type='senha' name='senha' id='senha' required>
                 <br>
-                <input type='submit' name='botao' value='Sign in'>
+                <input type='submit' name='botao' value='Entrar'>
                 <br>
             </form>
             <div class="botao">
-                <a href="formCad.php">Sign Up</a>
+                <a href="formCad.php">Cadastrar</a>
             </div>
 </body>
 </html>
