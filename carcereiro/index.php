145b6a42d70ed5bea3b18985156df1f35e89f3b9
Otávio Maldaner <otaviomaldaner571@gmail.com>
2022-12-01 14:26:03 -0300

    login page

---
+++ carcereiro/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="login-page">
+        <div class="login-page-util">
+            <div class="login-form">
+                <form action="">
+                    <h1>Login</h1>
+                    <div class="login-form-camps">
+                        <input type="text" name="login" id="login" placeholder="Insira o seu login" class="login-input">
+                        <input type="password" name="password" id="passeord" placeholder="Insira a sua senha" class="login-input">
+                    </div>
+                    <div class="form-submit-button">
+                        <input type="submit" value="Enviar" class="submit-button">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
