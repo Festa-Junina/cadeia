458c3ab14b0c6f713335a5d722e74ccadc5478d3
Otávio Maldaner <otaviomaldaner571@gmail.com>
2022-12-01 14:57:34 -0300

    upload

--- login/index.php
+++ login/index.php
@@ -17,12 +17,22 @@
                         <input type="text" name="login" id="login" placeholder="Insira o seu login" class="login-input">
                         <input type="password" name="password" id="passeord" placeholder="Insira a sua senha" class="login-input">
                     </div>
+                    <div class="recover-password-area">
+                        <a href="#">
+                            <span class="recover-password">Recuperar senha</span>
+                        </a>
+                    </div>
                     <div class="form-submit-button">
                         <input type="submit" value="Enviar" class="submit-button">
                     </div>
                 </form>
             </div>
         </div>
+        <footer class="login-page-footer">
+            <div class="footer-login-util-area">
+
+            </div>
+        </footer>
     </div>
 </body>
 </html>
