75e9147df9ad4e5a9e028f47ec8a63065edcafc3
Fábio Júnior Nielsson Bastian <fabiobastian44@gmail.com>
2022-12-07 11:59:14 -0300

    up

--- src/emitirOrdem/emitirOrdem.php
+++ src/emitirOrdem/emitirOrdem.php
@@ -23,32 +23,85 @@
 
     }
 
-
-    
 ?>
 
 
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="../../reset.css">
     <link rel="stylesheet" href="../../globalStyles.css">
-
     <title>Emitir Ordem de Prisão</title>
+    <style>
+        body{
+            width: 100vw;
+            height: 100vh;
+            color: var(--gray);
+            background-color: var(--primary);
+        }
+        .container{
+            margin: 0 auto;
+            width: 90%;
+            max-width: 480px;
+        }
+        .header{
+            position: sticky;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 10%;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
+        .main{
+            display: flex;
+            height: 90%;
+            flex-direction: column;
+            justify-content: space-around;
+        }
+        .form{
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: space-around;
+        }
+        .button{
+            height: 3em;
+            background-color: var(--green);
+            display: flex;
+            border-radius: 5px;
+            align-items: center;
+            justify-content: center;
+            min-width: 120px;
+        }
+        .button-header{
+            height: 2em;
+            background-color: var(--primaryVariant);
+        }
+        input{
+            border-radius: 3px;
+        }
+        /*DETALHES*/
+        h1{
+            color: var(--white);
+        }
+    </style>
 </head>
 
-<body>
-    <div class="centro">
-        <form action="emitirOrdem.php" method="post">
+<body class="container">
+    <header class="header">
+        <h1>Emitir Ordem de Prisão</h1>
+    </header>
+    <main class="main">
+        <form action="emitirOrdem.php" method="post" class="form">
 
             <label for="nomeMeliante">Nome do Meliante:</label>
-            <input type="text" name="nomeMeliante" id="nomeMeliante" required>
+            <input type="text" name="nomeMeliante" id="nomeMeliante" placeholder="ex: João da Silva Morais" required>
 
             <label for="tipoMeliante">Tipo do Meliante:</label>
             <select name="tipoMeliante" id="tipoMeliante" required>
@@ -81,23 +134,30 @@
             </select>
             <!--  -->
 
-
             <label for="descricaoMeliante">Descrição do Meliante:</label>
-            <textarea name="descricaoMeliante" id="descricaoMeliante" cols="30" rows="10" required></textarea>
+            <textarea name="descricaoMeliante" id="descricaoMeliante" cols="30" rows="10" required>
+ex-cabelo: 
+
+ex-parte superior: 
+
+ex-parte inferior: 
+
+ex-tênis: 
+
+            </textarea>
 
             <label for="localVisto">Ultimo local visto:</label>
-            <input type="text" name="localVisto" id="localVisto" required>
+            <input type="text" name="localVisto" id="localVisto" placeholder="ex: Em frete a praça principal" required>
 
             <label for="nomeDenunciante">Seu nome:</label>
-            <input type="text" name="nomeDenunciante" id="nomeDenunciante" required>
+            <input type="text" name="nomeDenunciante" id="nomeDenunciante" placeholder="ex: Elías dos Anjos" required>
 
             <label for="telefoneDenunciante">Seu telefone:</label>
-            <input type="tel" name="telefoneDenunciante" id="telefoneDenunciante" required>
+            <input type="tel" name="telefoneDenunciante" id="telefoneDenunciante" placeholder="(51) 9 98536256" required>
 
-            <input type="submit" name="submit" value="Enviar">
+            <input type="submit" name="submit" value="Enviar" class="button">
 
         </form>
-    </div>
+    </main>
 </body>
-
 </html>
--- src/emitirOrdem/validarTicket.php
+++ src/emitirOrdem/validarTicket.php
@@ -1,18 +1,3 @@
-<?php 
-
-
-require_once "../classes/Ticket.php";
-
-if(isset($_POST['submit'])){
-    $ticket = new Ticket($_POST['ticket']);
-    
-    if($ticket->autenticar()){
-        header("location: emitirOrdem.php");
-    }else{
-        echo "Ticket invalido ou já utilizado";
-    }
-}
-?>
 
 
 <!DOCTYPE html>
@@ -21,11 +6,80 @@ if(isset($_POST['submit'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Insira se Ticket</title>
+    <link rel="stylesheet" href="../../reset.css">
+    <link rel="stylesheet" href="../../globalStyles.css">
+    <title>Insira seu Ticket</title>
+    <style>
+        body{
+            width: 100vw;
+            height: 100vh;
+            color: var(--gray);
+            background-color: var(--primary);
+        }
+        .container{
+            margin: 0 auto;
+            width: 90%;
+            max-width: 480px;
+        }
+        .header{
+            position: sticky;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 10%;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
+        .main{
+            display: flex;
+            height: 90%;
+            flex-direction: column;
+            justify-content: space-around;
+        }
+        .form{
+            height: 60%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: space-around;
+        }
+        .button{
+            height: 3em;
+            background-color: var(--green);
+            display: flex;
+            border-radius: 5px;
+            align-items: center;
+            justify-content: center;
+            min-width: 120px;
+        }
+        /*DETALHES*/
+        h1{
+            color: var(--white);
+        }
+    </style>
 </head>
-<body>
-    <main>
-        <form action="validarTicket.php" method="post">
+<body class="container">
+    <header class="header">
+        <!-- ALGO -->
+    </header>
+    <main class="main">
+        <form action="validarTicket.php" method="post" class="form">
+
+            <?php 
+            require_once "../classes/Ticket.php";
+
+            if(isset($_POST['submit'])){
+                $ticket = new Ticket($_POST['ticket']);
+
+                if($ticket->autenticar()){
+                    header("location: emitirOrdem.php");
+                }else{
+                    echo "Ticket invalido ou já utilizado";
+                }
+            }
+            ?>
+
             <label for="ticket">Insira seu Ticket</label>
             <input type="number" id="ticket" name="ticket" required>
             <input type="submit" value="Validar Ticket" name="submit">
