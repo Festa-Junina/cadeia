441ca36b7f313b81354f256cec0ce27d289e9dd4
jeanHoffelder <104578531+jeanHoffelder@users.noreply.github.com>
2022-12-08 14:46:37 -0300

    up

--- cadastrarCarc.php
+++ cadastrarCarc.php
@@ -1,35 +1,13 @@
 <?php
 require_once __DIR__."/vendor/autoload.php";
 if(isset($_POST['submit'])){
-    $carc = new Carcereiro($_POST['email']);
-    $carc->setcodCarc($_POST['codCarc']);
-    $carc->setnomeCarc($_POST['nomeCarc']);
-    $carc->settelCarc($_POST['telCarc']);
+    $carc = new Carcereiro($_POST['emailCarc']);
+    $carc->setSenhaCarc($_POST['senha']);
+    $carc->setNomeCarc($_POST['nomeCarc']);
+    $carc->setTelCarc($_POST['telCarc']);
     $carc->save();
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastrar Carcereiro</title>
-</head>
-<body>
-<form action='formCad.php' method='POST'>
-        Código do Carcereiro: <input name='codCarc' type='int' required>
-        <br>
-        Nome do Carcereiro: <input name='nomeCarc' type='String' required>
-        <br>
-        E-mail do Carcereiro: <input name='emailCarc' type='email' required>
-        <br>
-        Telefone do Carcereiro: <input name='telCarc' type='int' required>
-        <input type="submit" value="Registrar Carcereiro" name='submit'>
-</body>
-</html>
--------------------------------------------------------------------------------------------------------------
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -53,15 +31,8 @@ if(isset($_POST['submit'])){
                 <input type='email' name='emailCarc' id='email' required><br>
                 <label for='senha'>Senha:</label><br>
                 <input type='password' name='senha' id='senha' required><br>
-                <div class="selects">
-                    <label for="funcao">Função:</label><br>
-                    <select name="funcao" id="funcao">
-                        <option value="Carcereiro">Carcereiro</option>
-                    </select><br>
-                    <label for="status">Status:</label><br>
-                    <select name="status" id="status">
-                        <option value="Ativo">Ativo</option>
-                    </select><br>
+                <label for='telefone'>Senha:</label><br>
+                <input type='int' name='telCarc' id='telCarc' required><br>
                 </div>
                 <div class="botaoCad">
                     <button name='botao' value='Cadastrar'>Cadastrar</button>
