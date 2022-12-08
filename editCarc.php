a6d3e3522e2cc86ec48ccc8c08bd8a5f78244710
Nícolas Augusto Köhler Arend <107890287+NicolasIFRS@users.noreply.github.com>
2022-12-08 11:32:52 -0300

    Update editCarc.php

--- editCarc.php
+++ editCarc.php
@@ -1,36 +1,60 @@
 <?php
-if(isset($_GET['codCarc'])){
-    require_once __DIR__."/vendor/autoload.php";
-    $carc = Carcereiro::find($_GET['codCarc']);
+if(isset($_GET['id'])){
+    require_once __DIR__."/src/Carcereiro.php";
+    $carcereiro = Carcereiro::find($_GET['id']);
 }
-if(isset($_POST['submit'])){
-    require_once __DIR__."/vendor/autoload.php";
-    $carc = new Carcereiro($_POST['codCarc'],$_POST['nomeCarc'],$_POST['emailCarc'],$_POST['telCarc']);
-    $carc->setIdCarc($_POST['codCarc']);
-    $carc->save();
+if(isset($_POST['botao'])){
+    require_once __DIR__."/src/Carcereiro.php";
+    $carcereiro = new Carcereiro($_POST['login'],$_POST['senha'],$_POST['status'],$_POST['funcao']);
+    $carcereiro->setIdUsuario($_POST['id']);
+    $carcereiro->save();
+    header("location: index.php");
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.form.scss">
     <title>Editar Carcereiro</title>
 </head>
 <body>
-
-
-    <form action="editCad.php" method="POST">
-            Código do Carcereiro: <input name='codCarc' type='int' required>
-            <br>
-            Nome do Carcereiro: <input name='nomeCarc' type='String' required>
-            <br>
-            E-mail do Carcereiro: <input name='emailCarc' type='email' required>
-            <br>
-            Telefone do Carcereiro: <input name='telCarc' type='int' required>
-            <input type='submit' value='Salvar Edição' name='submit'>
+    <section>   
+        <div class="divTitulo">
+            <h1 class="titulo">Editar Carcereiro</h1><br>
+        </div>
+    </section>
+        <br>
+    <section class="formulario"> 
+        <div class="divform">
+            <form class="formCad" action="editCarc.php" method="POST">
+                <?php
+                    echo "<label for='email'>E-mail:</label><br>";
+                    echo "<input name='login' id='login' value='{$carcereiro->getLogin()}' type='text' required>";
+                    echo "<br>";
+                    echo "<input name='id' value={$carcereiro->getIdUsuario()} type='hidden'>";
+                ?>
+                <label for='senha'>Senha:</label><br>
+                <input type='password' name='senha' id='senha' required><br>
+                <div class="selects">
+                    <label for="funcao">Função:</label><br>
+                    <select name="funcao" id="funcao">
+                        <option value="Carcereiro">Carcereiro</option>
+                    </select><br>
+                    <label for="status">Status:</label><br>
+                    <select name="status" id="status">
+                        <option value="Ativo">Ativo</option>
+                    </select><br>
+                </div>
+                <div class="botaoCad">
+                    <button name='botao' value='Cadastrar'>Cadastrar</button>
+                </div>
             </form>
+            <a href='index.php'>voltar ao inicio </a>
+        </div>
+    </section>
+    </form>
 </body>
 </html>
