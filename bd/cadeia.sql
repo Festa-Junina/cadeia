a3ec02b564c3394a1f5c7941e73133a3414f8caf
Cauê Mayolo da Silveira <104577850+CaueMayolo@users.noreply.github.com>
2022-12-09 14:59:27 -0300

    Banco de dados novo - teste

--- bd/cadeia.sql
+++ bd/cadeia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 01-Dez-2022 às 20:54
+-- Tempo de geração: 09-Dez-2022 às 18:58
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.0.15
 
@@ -24,35 +24,65 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `policial`
+-- Estrutura da tabela `funcao`
 --
 
-CREATE TABLE `policial` (
+CREATE TABLE `funcao` (
+  `idFuncao` int(10) NOT NULL,
+  `nome` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
   `idUsuario` int(10) NOT NULL,
   `login` varchar(200) NOT NULL,
   `senha` varchar(200) NOT NULL,
-  `telefone` varchar(50) NULL,
+  `telefone` varchar(50) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `ativo` tinyint(1) NOT NULL,
+  `idFuncao` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`idUsuario`, `login`, `senha`, `telefone`, `nome`, `ativo`, `idFuncao`) VALUES
+(8, 'mayolo.caue@gmail.com', '12345', '51998158025', 'Cauê Mayolo da Silveira', 0, 2),
+(9, 'lucas@gmail.com', '$2y$10$4/N0gKJoMeLqQ2OlFlzjV.BdNI9UOjlmVj4quk8BsVbJkKQDkeIDy', '51998158000', 'Lucas Becker', 1, 0);
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices para tabela `policial`
+-- Índices para tabela `funcao`
 --
-ALTER TABLE `policial`
-  ADD PRIMARY KEY (`idUsuario`);
+ALTER TABLE `funcao`
+  ADD PRIMARY KEY (`idFuncao`);
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`),
+  ADD UNIQUE KEY `fk_idUsuario` (`idFuncao`),
+  ADD KEY `idFuncao` (`idFuncao`);
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT de tabela `policial`
+-- AUTO_INCREMENT de tabela `usuario`
 --
-ALTER TABLE `policial`
-  MODIFY `idUsuario` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
