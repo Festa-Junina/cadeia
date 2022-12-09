7160d89a39e53382542213fd7b2312f1cca07f78
Marcelo <107191197+marselosd@users.noreply.github.com>
2022-12-09 11:22:53 -0300

    UP

--- src/emitirOrdem/emitirOrdem.php
+++ src/emitirOrdem/emitirOrdem.php
@@ -1,33 +1,33 @@
-<?php 
+<?php
 
-    require_once "../db/MySQL.php";
-    require_once "../classes/OrdemPrisao.php";
-    session_start();
+require_once "../db/MySQL.php";
+require_once "../classes/OrdemPrisao.php";
+session_start();
 
-    if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
 
-        $ordemPrisao = new OrdemPrisao($_POST['nomeMeliante'],$_POST['descricaoMeliante'],$_POST['localVisto'],$_POST['nomeDenunciante'],$_POST['telefoneDenunciante']);
-        
-        $ordemPrisao->setIdTicket($_SESSION['idTicket']);
-        
-        $ordemPrisao->setIdTipoMeliante($_POST['tipoMeliante']);
+    $ordemPrisao = new OrdemPrisao($_POST['nomeMeliante'], $_POST['descricaoMeliante'], $_POST['localVisto'], $_POST['nomeDenunciante'], $_POST['telefoneDenunciante']);
 
-        if(isset($_POST['turmaMeliante'])){
-            $ordemPrisao->setIdTurmaMeliante($_POST['turmaMeliante']);
-        }
+    $ordemPrisao->setIdTicket($_SESSION['idTicket']);
 
-        $ordemPrisao->save();
+    $ordemPrisao->setIdTipoMeliante($_POST['tipoMeliante']);
 
-        //mensagem de sucesso?
-        header("location: ../../index.html");
-
+    if (isset($_POST['turmaMeliante'])) {
+        $ordemPrisao->setIdTurmaMeliante($_POST['turmaMeliante']);
     }
+
+    $ordemPrisao->save();
+
+    //mensagem de sucesso?
+    header("location: ../../index.html");
+}
 
 ?>
 
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -56,29 +56,29 @@
             <select name="tipoMeliante" id="tipoMeliante" required>
                 <option value="" disabled selected>Escolha um tipo</option>
 
-                    <?php 
-                        $conexao = new MySQL();
-                        $sql = "SELECT * FROM tipomeliante";
-                        $tiposMeliantes = $conexao->consulta($sql);
-                        foreach($tiposMeliantes as $tipo){
-                            echo "<option value='{$tipo['idTipoMeliante']}'> {$tipo['nome']} </option>";
-                        }
-                    ?>  
+                <?php
+                $conexao = new MySQL();
+                $sql = "SELECT * FROM tipomeliante";
+                $tiposMeliantes = $conexao->consulta($sql);
+                foreach ($tiposMeliantes as $tipo) {
+                    echo "<option value='{$tipo['idTipoMeliante']}'> {$tipo['nome']} </option>";
+                }
+                ?>
             </select>
 
             <!-- SO ABRE SE FOR ALUNO -->
             <label for="turmaMeliante">Turma do Meliante: </label>
             <select name="turmaMeliante" required id="turmaMeliante">
                 <option value="" disabled selected>Escolha uma turma</option>
-   
-                <?php 
-                    $conexao = new MySQL();
-                    $sql = "SELECT * FROM turmameliante";
-                    $turmaMeliantes = $conexao->consulta($sql);
-                    foreach($turmaMeliantes as $turma){
-                        echo "<option value='{$turma['idTurmaMeliante']}'> {$turma['nome']} </option>";
-                    }
-                ?> 
+
+                <?php
+                $conexao = new MySQL();
+                $sql = "SELECT * FROM turmameliante";
+                $turmaMeliantes = $conexao->consulta($sql);
+                foreach ($turmaMeliantes as $turma) {
+                    echo "<option value='{$turma['idTurmaMeliante']}'> {$turma['nome']} </option>";
+                }
+                ?>
 
             </select>
             <!--  -->
@@ -100,5 +100,15 @@
         </form>
     </main>
     <script src="script.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.12/jquery.mask.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.12/jquery.mask.min.js"></script>
+    <script>
+        //Mascara TEL
+        $(document).ready(function() {
+            $('#telefoneDenunciante').mask('(99) 9 9999-9999');
+        });
+    </script>
 </body>
+
 </html>
