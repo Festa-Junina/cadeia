8c4bbf3a336e0d56908515a7a7b0d6c17cab5f9d
Nícolas Augusto Köhler Arend <107890287+NicolasIFRS@users.noreply.github.com>
2022-12-09 08:52:15 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,46 +1,49 @@
 <?php
 require_once __DIR__."/vendor/autoload.php";
-$carcereiros = Carcereiro::findal();
+$carcereiros = Carcereiro::findall();
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Carcereiro</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Listar Carcereiros</title>
 </head>
 <body>
-    <div class="formulario">
-    <table>
+    
+<hr>
+<h1>Página Inicial - Carcereiro</h1>
+<hr>
+
+<table>
     <tr>
-        <th>Login</th>
-        <th>Telefone</th>
         <th>Nome</th>
-        <th class="linha"><img src="img/ops.png" alt="">OPÇÕES</th>
+        <th>Telefone</th>
+        <th>Login</th>
     </tr>
     <?php
     foreach($carcereiros as $carcereiro){
         echo "<tr>";
-        echo "<td>{$carcereiro->getLogin()}</td>";
-        echo "<td>{$carcereiro->getTelefone()}</td>";
-        echo "<td>{$carcereiro->getNome()}</td>";
-        echo "<td>
-                <a href='formEdit.php?id={$carcereiro->getIdUsuario()}'>Editar</a>
-                <a href='excluir.php?id={$carcereiro->getIdUsuario()}'>Excluir</a> 
-            </td>";
+        echo "<td>{$carcereiro->getNomeCarc()}</td>";
+        echo "<td>{$carcereiro->getTelCarc()}</td>";
+        echo "<td>{$carcereiro->getEmailCarc()}</td>";
     }
     ?>
 </table>
-            <div class="botao">
-                <a href="CadastrarCarc.php">Cadastrar</a>
-            </div>
 
-            <div class="botao">
-                <a href="editCarc.php">Editar CARC teste</a>
-            </div> 
+
+    <div class="links" >
+        <div class="as">
+            <a href='cadastrarCarc.php'>Cadastrar Carcereiro</a>
+        </div>
+</div>
 </body>
 </html>
+
+
+
+
 
 
