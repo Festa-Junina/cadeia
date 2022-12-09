d3973203726dc2a73c3404c0b00a9a0b24c6594e
Nícolas Augusto Köhler Arend <107890287+NicolasIFRS@users.noreply.github.com>
2022-12-09 07:33:13 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,76 +9,10 @@ if(isset($_POST['botao'])){
         header("location: index.php");
     }
 }
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Carcereiro</title>
-</head>
-<body>
-    <div class="formulario">
-        <form action="index.php" method='post'>
-                <label for='email'>E-mail:</label>
-                <br>
-                <input type='email' name='email' id='email' required>
-                <br>
-                <label for='senha'>Senha:</label>
-                <br>
-                <input type='password' name='senha' id='senha' required>
-                <br>
-                <input type='submit' name='botao' value='Entrar'>
-                <br>
-            </form>
-            <div class="botao">
-                <a href="CadastrarCarc.php">Cadastrar</a>
-            </div>
-
-            <div class="botao">
-                <a href="editCarc.php">Editar CARC teste</a>
-            </div> 
-</body>
-</html>
-
-
-
-
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Codespaces
-Marketplace
-Explore
- 
-@NicolasIFRS 
-Festa-Junina
-/
-cadeia
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-cadeia/index.php /
-@CaueMayolo
-CaueMayolo Refatoração segundo o banco de dados
-Latest commit ffe26d1 in 5 hours
- History
- 1 contributor
-50 lines (48 sloc)  1.65 KB
-
-<?php
 require_once __DIR__."/vendor/autoload.php";
-require_once __DIR__."/src/Policial.php";
-$policiais = Policial::findall();
+require_once __DIR__."/src/Carcereiro.php";
+$carcereiros = Carcereiro::findall();
 ?>
 
 <!DOCTYPE html>
@@ -88,13 +22,13 @@ $policiais = Policial::findall();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>main policial</title>
+    <title>Home Carcereiro</title>
 </head>
 
 <body>
     <article>
         <div>
-            <a class="link-cadastro" href='formPolicial.php'>Registrar policial</a>
+            <a class="link-cadastro" href='cadastrarCarc.php'>Registrar Carcereiro</a>
         </div>
     </article>
     <article>
@@ -109,14 +43,14 @@ $policiais = Policial::findall();
             </thead>
             <tbody>
             <?php
-                foreach($policiais as $policial){
+                foreach($carcereiros as $carcereiro){
                     echo "<tr>";
-                    echo "<td>{$policial->getLogin()}</td>";
-                    echo "<td>{$policial->getTelefone()}</td>";
-                    echo "<td>{$policial->getNome()}</td>";
+                    echo "<td>{$carcereiro->getLogin()}</td>";
+                    echo "<td>{$carcereiro->getTelefone()}</td>";
+                    echo "<td>{$carcereiro->getNome()}</td>";
                     echo "<td>
-                            <a href='formEdit.php?id={$policial->getIdUsuario()}'>Editar</a>
-                            <a href='excluir.php?id={$policial->getIdUsuario()}'>Excluir</a> 
+                            <a href='formEdit.php?id={$carcereiro->getIdUsuario()}'>Editar</a>
+                            <a href='excluir.php?id={$carcereiro->getIdUsuario()}'>Excluir</a> 
                         </td>";
                     echo "</tr>";
                 }
@@ -125,18 +59,4 @@ $policiais = Policial::findall();
     </article>
 </body>
 </html>
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-cadeia/index.php at omega_crud-policial · Festa-Junina/cadeia
+?>
