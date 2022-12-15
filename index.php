559c3cde618ab614bb817466e04257c968399edf
Nícolas Augusto Köhler Arend <107890287+NicolasIFRS@users.noreply.github.com>
2022-12-15 01:08:01 -0300

    style

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $carcereiros = Carcereiro::findall();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style_carcereiro.css">
     <title>Listar Carcereiros</title>
 </head>
 <body>
--- listarCarc.php
+++ listarCarc.php
@@ -9,7 +9,7 @@ $carcereiros = Carcereiro::findall();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style_carcereiro.css">
     <title>Listar Carcereiros</title>
 </head>
 <body>
@@ -22,7 +22,7 @@ $carcereiros = Carcereiro::findall();
 
     <div class="links" >
         <div class="add">
-            <a href="cadastrarCarc.php">Cadastrar Carcereiro</a>
+            <a href="cadastrarCarc.php">+ Cadastrar Carcereiro</a>
         </div>
     </div>
     <div class="box-dados-carc">
