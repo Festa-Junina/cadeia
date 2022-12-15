a3c7e9ab8523517663c37ebb91937e811f615886
Matheus Persch <matheusp.estudos@gmail.com>
2022-12-15 09:22:26 -0300

    Update index.php

--- src/listarDetentos/index.php
+++ src/listarDetentos/index.php
@@ -182,9 +182,7 @@ foreach ($detentos as $detento) {
                     $diff = $today - $prison;
 
                     $minutes = round(abs($diff / 60), 0);
-
                     echo "</div>";
-
                     echo "<p style=\"text-align: right; font-size: 14px\">Preso há {$minutes} minutos</p>";
                 echo "</div>";
             }
