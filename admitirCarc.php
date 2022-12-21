7a969b17f484ac078090e4f6c8d0b714500b8814
Nícolas Augusto Köhler Arend <nicolas.arend@aluno.feliz.ifrs.edu.br>
2022-12-21 08:32:32 -0300

    funcional

--- admitirCarc.php
+++ admitirCarc.php
@@ -3,4 +3,4 @@ require_once __DIR__."/vendor/autoload.php";
 require_once __DIR__."/src/Carcereiro.php";
 $carc = Carcereiro::find($_GET['id']);
 $carc->admitir();
-header("location: index.php");
+header("location: listarCarc.php");
--- demitirCarc.php
+++ demitirCarc.php
@@ -3,4 +3,4 @@ require_once __DIR__."/vendor/autoload.php";
 require_once __DIR__."/src/Carcereiro.php";
 $carc = Carcereiro::find($_GET['id']);
 $carc->demitir();
-header("location: index.php");
+header("location: listarCarc.php");
