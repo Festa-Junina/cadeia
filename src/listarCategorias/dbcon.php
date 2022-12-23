a65b932dd3a694d7bfab9a21d9cf8275e003b7f8
Nícolas Augusto Köhler Arend <107890287+NicolasIFRS@users.noreply.github.com>
2022-12-23 14:10:12 -0300

    Update dbcon.php

--- src/listarCategorias/dbcon.php
+++ src/listarCategorias/dbcon.php
@@ -1,7 +1,7 @@
 <?php
 require_once "../login/sessions/sessaoAdmin.php";
 
-$conn = mysqli_connect("localhost","aluno","aluno","cadeia-app");
+$conn = mysqli_connect("localhost","root","","cadeia-app");
 
 if(!$conn){
     die('Connection Failed'. mysqli_connect_error());
